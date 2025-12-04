105e5d3a0e257189d17adcc402176b7bc38233c5
andre <araujo.andre.direito@gmail.com>
2025-12-04 17:00:42 -0300

    atualização do index para desafio

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
 
-    return <h1> Olha, este é o início do meu projeto :) </h1>
+    return <h1> Olha, este é o início do meu projeto :) Queria mostrar ele primeiro pra ti </h1>
 }
 
 export default Home;
